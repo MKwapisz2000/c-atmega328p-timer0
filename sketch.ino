dbe2e1e098b2788c4fe4ae0747ff05322312f6bf
MKwapisz <164620906+MKwapisz2000@users.noreply.github.com>
2025-02-13 12:21:31 +0100

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,53 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void Timer0_init()
+{
+  //tryb pracy CTC
+  TCCR0A |= (1<<WGM01);
+
+  //prescaler 1024 -> (16 000 000 /(1024*1000)) = 15,62 -> 1ms
+  TCCR0B |= (1<<CS02);
+  TCCR0B |= (1<<CS00);
+  
+  //porównywacz - 1ms
+  OCR0A = 15;
+
+  //włączenie przerwań 
+  TIMSK0 |= (1<<OCIE0A);
+ 
+  //wlaczenie przerwań 
+  sei();
+}
+
+
+volatile int licznik =0;
+int main()
+{
+  //diody
+  DDRD |= (1<<PD5);
+  DDRD |= (1<<PD3);
+  DDRD |= (1<<PD4);
+
+  PORTD &= ~(1<<PD5);
+  PORTD &= ~(1<<PD3);
+  PORTD &= ~(1<<PD4);
+
+  Timer0_init();
+  
+  while(1)
+  {}
+
+  return 0;
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+  licznik++;
+  if(licznik==1000)
+  {
+    // Prosty kod debugujący do sprawdzenia działania przerwań
+    PORTD ^= (1<<PD5); // Zmiana stanu diody
+    licznik=0;
+  }
+}
